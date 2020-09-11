84e8dec0e6b7255912b3a4e32657c1d8c1de209d
Rogers Macharia <rogersmacharia7@gmail.com>
2020-09-11 16:01:03 +0300

    added the Book and Author models

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+GENRE = [
+    ("PF", "Personal Finance"),
+    ("Sci-Fi", "Science Fiction"),
+    ("Tech", "Technology"),
+    ("Prog", "Computer Programming"),
+    ("PG", "Personal Growth")
+]
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+
+
+class Book(models.Model):
+    author = models.ManyToManyField(Author, related_name="books")
+    title = models.CharField(max_length=100, blank=True, null=True)
+    genre = models.CharField(choices=GENRE, max_length=100, blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.CharField(max_length=1024, blank=True, null=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+    def get_authors(self):
+        return self.author.all()

ac36cfb2695029ae2ee909a54080d954ce970c0a
Rogers Macharia <rogersmacharia7@gmail.com>
2020-09-11 21:11:42 +0300

    added the Order and OrderItem models and tested them: OK

--- bookstore/tests/test_models.py
+++ bookstore/tests/test_models.py
@@ -1,13 +1,29 @@
 from django.test import TestCase
-from ..models import Author, Book
+from ..models import *
+from django.contrib.auth.models import User
 
 
-class TestAuthorModel(TestCase):
+class TestModelsDefaults(TestCase):
     def setUp(self):
-        self.author = Author.objects.create(
-            first_name='Rogers',
-            last_name='Macharia',
+        User.objects.create_user(
+            first_name="Rogers",
+            last_name="Macharia",
+            password="1234",
+            username="rogers",
+            email="rogersmacharia@gmail.com"
         )
+        Book.objects.create(title="Atomic Habits", price=100.00, genre="PG", description="Small habits, big difference")
+        Author.objects.create(first_name="James", last_name="Clear")
+        self.user = User.objects.get(pk=1)
+        self.author = Author.objects.get(pk=1)
+        self.book = Book.objects.get(pk=1)
+        self.book.author.add(self.author)
+        Order.objects.create(user=self.user)
+
+
+class TestAuthorModel(TestModelsDefaults):
+    def setUp(self):
+        super().setUp()
 
     def test_object_saved(self):
         author = Author.objects.get(pk=1)
@@ -16,18 +32,31 @@ class TestAuthorModel(TestCase):
         self.assertEquals(author.pk, 1)
 
 
-class TestBookModel(TestAuthorModel, TestCase):
+class TestBookModel(TestModelsDefaults):
     def setUp(self):
-        super(TestBookModel, self).setUp()
-        self.book = Book.objects.create(
-            title="Atomic Habits",
-            genre="PG",
-            price=100,
-        )
+        super().setUp()
         self.book.author.add(self.author)
 
     def test_object_saved(self):
-        book = Book.objects.get(pk=1)
-        self.assertEquals(book.title, self.book.title)
-        self.assertEquals(book.get_genre_display(), "Personal Growth")
-        self.assertEquals(list(book.get_authors()), [self.author])
+        self.assertEquals(self.book.title, "Atomic Habits")
+        self.assertEquals(self.book.get_genre_display(), "Personal Growth")
+        self.assertEquals(list(self.book.get_authors()), [self.author])
+
+
+class TestOrderAndOrderItemModel(TestModelsDefaults):
+    def setUp(self):
+        super().setUp()
+        self.order, created = Order.objects.get_or_create(user=self.user, complete=False)
+        self.order_item, created = OrderItem.objects.get_or_create(user=self.user, order=self.order, book=self.book)
+        self.order_item.quantity += 10
+        self.order_item.save()
+
+    def test_object_saved(self):
+        self.assertEquals(self.order.user.first_name, "Rogers")
+        self.assertEquals(self.order.complete, False)
+        self.assertEquals(list(self.order.orderitem_set.all()), [self.order_item])
+        self.assertEquals(self.order_item.quantity, 10)
+        self.assertEquals(self.order_item.order, self.order)
+        self.assertEquals(self.order_item.book, self.book)
+        self.assertEquals(self.order_item.user.username, "rogers")
+        self.assertEquals(self.order_item.book, self.book)
